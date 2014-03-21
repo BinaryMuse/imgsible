94bd837c2eecdb2365c2670f179f6c220628cc8e
Brandon Tilley <brandon@brandontilley.com>
2014-03-20 20:49:53 -0700

    Refresh image list placeholder images

--- public/js/components/image_list.jsx
+++ public/js/components/image_list.jsx
@@ -2,27 +2,25 @@
 
 var ImageList = React.createClass({
   render: function() {
+    var images = ['7pt', '7pu', '7pv', '7pw', '7px'];
+
+    var imageEls = images.map(function(img) {
+      return (
+        <div className='image-list-item' key={img}>
+          <a href={'/image/' + img}>
+            <img src={'http://localhost:5000/i/' + img + '-t.gif'} />
+          </a>
+        </div>
+      )
+    });
+
     return (
       <div id='image-list'>
         <div className='selector'>
           Showing the most [ <a href='#' className='active'>recent</a> / <a href='#'>voted</a> ] images
         </div>
 
-        <div className='image-list-item'>
-          <a href='/image/7q5'>
-            <img src='http://localhost:5000/i/7q5-t.gif' />
-          </a>
-        </div>
-        <div className='image-list-item'>
-          <a href='/image/7qc'>
-            <img src='http://localhost:5000/i/7qc-t.gif' />
-          </a>
-        </div>
-        <div className='image-list-item'>
-          <a href='/image/7qcasdf'>
-            <img src='http://localhost:5000/i/7qc-t.gif' />
-          </a>
-        </div>
+        {imageEls}
       </div>
     );
   }
